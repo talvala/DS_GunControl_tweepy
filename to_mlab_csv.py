882eee1db889417999ddbf80c5814e0f2c82e823
talvala <valanit90@gmail.com>
2019-01-05 17:47:34 +0200

    1. data analysis files 2. Mlab upload scripts files

---
+++ to_mlab_csv.py
@@ -0,0 +1,34 @@
+from pymongo import MongoClient
+
+import json
+
+client = MongoClient('mongodb://.../bigdata')
+mydb = client.bigdata
+
+
+def add_country(state,population,precentage,opinion,governor,governor_opinion):
+    mydb.countries.insert_one(
+            {
+            "state" : state,
+            "population" : population,
+            "precentage" : precentage,
+            "opinion" : opinion,
+            "governor" : governor,
+            "governor_opinion" : governor_opinion
+
+            })
+
+jsonString = open("countries.json", 'r')
+jsonObject = json.load(jsonString)
+for line in jsonObject:
+
+    state = line['state']
+    population = line['population']
+    precentage = line['precentage']
+    opinion = line['opinion']
+    governor = line['governor']
+    governor_opinion = line['governor_opinion']
+    add_country(state,population,precentage,opinion,governor,governor_opinion)
+
+
+
--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,24 +1,43 @@
-from tweepy.streaming import StreamListener
-from tweepy import OAuthHandler
-from tweepy import Stream
+import tweepy
+import json
 
-from twitter_credentials import CONSUMER_KEY,CONSUMER_KEY_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET
+# Specify the account credentials in the following variables:
+consumer_key = '...'
+consumer_secret = '...'
+access_token = '...'
+access_token_secret = '...'
 
-class StdOutListener(StreamListener):
-	def on_data(self, data):
-		print(data)
-		return True
 
-	def on_error(self, status):
-		print(status)
+# This listener will print out all Tweets it receives
+class PrintListener(tweepy.StreamListener):
+    def on_data(self, data):
+    # Decode the JSON data
+        data = json.loads(data)
+        print(data)
+        location = data['user']['location']
 
-if __name__ == "__main__":
-	listener = StdOutListener()
-	auth = OAuthHandler(CONSUMER_KEY, CONSUMER_KEY_SECRET)
-	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-	
-	stream = Stream(auth, listener)
-	
-	stream.filter(track=['second amendment','gun control','guns in america'])
+        if 'extended_tweet' in data:
+            if 'full_text' in data['extended_tweet']:
+                text = data['extended_tweet']['full_text']
+        else:
+            text = data['text']
 
-	
+    print_string = {{text}"{0}"{location}"{1}"}.format(text,location)
+    print(print_string)
+    #print (json.dumps({'text': text, 'location': location}))
+    
+    def on_error(self, status):
+        print(status)
+
+
+if __name__ == '__main__':
+    listener = PrintListener()
+
+
+# Authenticate
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
+
+# Connect the stream to our listener
+    stream = tweepy.Stream(auth, listener)
+    stream.filter(track=['second amendment','gun control','guns in america'])
