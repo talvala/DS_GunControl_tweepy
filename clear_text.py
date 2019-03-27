0cc8d2d52ce514a64528b9e692ebc95980338fd6
talvala <32924974+talvala@users.noreply.github.com>
2019-03-27 21:58:52 +0200

    Update clear_text.py

--- clear_text.py
+++ clear_text.py
@@ -1,19 +1,24 @@
 import re
-
 from pymongo import MongoClient
 
 
-class DBUtils(object):
+DB_NAME = 'bigdata'
+
+
+class TextCleaner(object):
 
     def __init__(self):
-        self._client = MongoClient('')
-        self._db = self._client.bigdata
-
+        self._db = self._get_db_by_name(DB_NAME)
+    
+    @staticmethod
+    def _get_db_by_name(db_name):
+        client = MongoClient('')
+        return client[db_name]
+    
     def clear_text(self):
         """
-            this method runs on all tweets and deletes the non letters or numbers characters
-            """
-
+        this method runs on all tweets and deletes the non letters or numbers characters
+        """
         for tweet in self._db.tweets.find():
             new_text = re.sub('[%]', '', tweet['text'])
             print("old text {}".format(tweet['text']))
@@ -22,5 +27,5 @@ class DBUtils(object):
 
 
 if __name__ == "__main__":
-    db_util = DBUtils()
-    db_util.clear_text()
+    cleaner = TextCleaner()
+    cleaner.clear_text()
